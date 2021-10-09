43d1e753310af8208988560a934cdae35c3bde8d
Pils48 <ha.rg@yandex.ru>
2021-10-09 15:59:13 +0300

    Add MSE calculation for modules functions.

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from sklearn.tree import RandomForestRegressor
+from sklearn.metrics import mse_score
+
+from module_a_dev import polynom_3
+from useful_package.module_b import hyperbola
+
+x = np.random.uniform(0, 1)
+y = polynom_3(x)
+
+model = RandomForestRegressor()
+model.fit(x, y)
+
+prediction = model.predict(x)
+print(mse_score(y, prediction))
